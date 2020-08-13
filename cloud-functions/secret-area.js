936ed239758c65d52640af354eebaf9dafa3962f
Flavius Bohaciuc <flaviusbohaciuc@gmail.com>
2020-08-13 15:13:44 +0100

    Fix typo.

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,4 +1,4 @@
-exports.hadler = function (event, context, callback) {
+exports.handler = function (event, context, callback) {
     callback(null, {
         statusCode: 200,
         body: 'Wellcome to the super secret area'
