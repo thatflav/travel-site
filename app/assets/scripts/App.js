eb38486f6a32b273e72096f2c4b147dd2dee00a0
Flavius Bohaciuc <flaviusbohaciuc@gmail.com>
2020-08-13 16:47:18 +0100

    Fix typo,again.../...

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,22 +1,22 @@
-import '../styles/styles.css';
-import 'lazysizes';
-import MobileMenu from './modules/MobileMenu';
-import RevealOnScroll from './modules/RevealOnScroll';
-import StickyHeader from './modules/StickyHeader';
-import ClientArea from './modules/ClientArea';
+import '../styles/styles.css'
+import 'lazysizes'
+import MobileMenu from './modules/MobileMenu'
+import RevealOnScroll from './modules/RevealOnScroll'
+import StickyHeader from './modules/StickyHeader'
+import ClientArea from './modules/ClientArea'
 
-new ClientArea();
-new StickyHeader();
-new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
-new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
+new ClientArea()
+new StickyHeader()
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
 new MobileMenu();
 let modal
 
-document.querySelectorAll('.open-modal').forEach(el => {
-  el.addEventListener('click', e => {
+document.querySelectorAll(".open-modal").forEach(el => {
+  el.addEventListener("click", e => {
     e.preventDefault()
-    if (typeof modal == 'undefined') {
-      import(/* webpackChunkName: "modal" */'./modules/Modal').then(x => {
+    if (typeof modal == "undefined") {
+      import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
         modal = new x.default()
         setTimeout(() => modal.openTheModal(), 20)
       }).catch(() => console.log("There was a problem."))
@@ -26,7 +26,6 @@ document.querySelectorAll('.open-modal').forEach(el => {
   })
 })
 
-
 if (module.hot) {
-  module.hot.accept();
+  module.hot.accept()
 }
